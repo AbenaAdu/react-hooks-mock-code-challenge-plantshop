68027c02ad0645bc28ba5388e48df68b28cbafed
Abena Adu <abeanadu@gmail.com>
2022-11-09 14:42:14 -0500

    commit tree 28f2971af8d2d429fd598383fc6183661d014c4e

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,16 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plantData }) {
+  
+  const eachPlant = plantData.map(plant => {
+    return <PlantCard 
+    key={plant.id}
+    plant={plant}
+    />
+  })
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{eachPlant}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+const [plantData, setPlantData] = useState([])
+const [searchPlant, setSearchPlant] = useState("")
+
+
+
+ useEffect(() => {
+  fetch("http://localhost:6001/plants")
+  .then(response => response.json())
+  .then(data => setPlantData(data))
+ }, [])
+
+ function handleAddNewPlant(newPlantObj) {
+  const newPlantArray = [...plantData, newPlantObj]
+  setPlantData(newPlantArray)
+ }
+ const filteredPlants = plantData.filter((plant) => {
+  if (searchPlant === "") {
+    return true 
+  } else return plant.name.toLowerCase().includes(searchPlant.toLowerCase())
+  
+ })
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm handleAddNewPlant={handleAddNewPlant}/>
+      <Search setSearchPlant={setSearchPlant} />
+      <PlantList key={plantData.id} plantData={filteredPlants} />
     </main>
   );
 }
